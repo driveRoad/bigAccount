f11dfef1221b03cde579869743d95a322f2683ed
chumo <chumo@chumodeMacBook-Pro.local>
2018-03-05 16:02:50 +0800

    fix conflict

--- app/controller/ventureedu/ventureedu.js
+++ app/controller/ventureedu/ventureedu.js
@@ -5,16 +5,7 @@ let ventureEduList = require('../../mocks/ventureedu_list_json');
 module.exports = app => {
   return class AppController extends app.Controller {
     async new() {
-<<<<<<< HEAD
       const { ctx } = this;
-      await ctx.render('ventureedu/new.js', {
-        menuActive:'venture',
-        title:'--react server side render--',
-        keywords:'react, server side render',
-        message: { text: '风险教育!'}});
-=======
-      const {ctx} = this;
-      console.log('xxxxxxx'+ventureEduList);
       await ctx.render('ventureedu/new.js', {
           menuActive: 'venture',
           title: '--react server side render--',
@@ -23,7 +14,6 @@ module.exports = app => {
           ventureEduList: ventureEduList
         }
       );
->>>>>>> b78f97fbd16e0036f4586fc28044a4b121fe6830
     }
 
     async ventureedu_detail() {
