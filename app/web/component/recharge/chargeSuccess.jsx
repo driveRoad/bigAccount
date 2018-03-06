5f4c7868fddd890201697b5e07f8980f94b364bd
chen54438 <chen54438@163.com>
2018-03-06 18:06:28 +0800

    add chargeSelect page

--- app/web/component/recharge/chargeSuccess.jsx
+++ app/web/component/recharge/chargeSuccess.jsx
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import BottomBanner from '../../asset/images/recharge/tuiBanner.png';
 import './chargeSuccess.css'
 
 export default class ChargeSuccess extends Component {
   constructor(props) {
     super(props);
+    this.onCheckAccount = this.onCheckAccount.bind(this);
   }
+
+
+  onCheckAccount() {
+    console.log('点击查看账户');
+  }
+
   render() {
     return (
       <div className="charge-success-container">
         <div className="charge-success-card">
           <div className="top-section">
             <p className="success-tint">您已成功充值1000.0元!</p>
-            <div className="check-account"><p>查看我的账户</p></div>
+
+            <button type="button" className="check-account" onClick={this.onCheckAccount}>查看我的账户</button>
           </div>
           <img src={BottomBanner} alt="charge-seccess"/>
         </div>
